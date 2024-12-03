5da58514d2d10fa179f706e6365e475e780cdb36
Victor <example@gamil.com>
2024-12-03 15:55:50 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+"use strict"
+
+let number = 5;
+const leftBorderWidth = 1;
+
+const result = confirm("Are you here?");
+console.log(result);
+const answer = prompt("Are you 18?", "18");
+console.log(answer);
+
+const answers= [];
+
+answers[0] = prompt('Whats your name?','');
+answers[1] = prompt('Whats your Family name?','');
+answer [2] = prompt('How old are you?', '');
+
+document.write(answers);
